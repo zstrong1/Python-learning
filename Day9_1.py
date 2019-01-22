447bf1d55c6ffb6c0047ae78eb372d6348ceda95
Zstrong <m15527370319@163.com>
2019-01-22 21:45:39 +0800

    updata Day9_1

--- Day9_1.py
+++ Day9_1.py
@@ -1,7 +1,66 @@
-# 题目一:略
-# 题目二:略
-# 题目三:略
-# 题目四：
+import time
+import random
+
+#题目1，2，3
+def auth(func):
+    print('Get in auth')
+    def wrapper1(*args,**kwargs):
+        name = input('user:>>')
+        password = input('password:>>')
+        if name == 'Albert' and password == '123':
+            print('login successful')
+            res = func(*args,**kwargs)
+            return res
+        else:
+            print('login failes')
+    return wrapper1
+
+
+
+
+def timer(func):
+    print('Get in timer')
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        print('start')
+        res = func(*args,**kwargs)
+        end_time = time.time()
+        print('Run Time is %s'%(end_time-start_time))
+        return res
+    return wrapper
+
+
+@timer
+@auth
+def foo():
+    time.sleep(random.randint(2,9))
+    print('Get in foo')
+
+foo()
+
+
+
+#
+# 5 编写装饰器，为多个函数加上认证功能，要求登录成功一次，在超时时间内无需重复登录，超过了超时时间，则必须重新登录
+#
+# 6 编写下载网页内容的函数，要求功能是：用户传入一个url，函数返回下载页面的结果
+#
+# 7 为题目五编写装饰器，实现缓存网页内容的功能：
+# 具体：实现下载的页面存放于文件中，如果文件内有值（文件大小不为0），就优先从文件中读取网页内容，
+# 否则，就去下载，然后存到文件中
+# 扩展功能：用户可以选择缓存介质/缓存引擎，针对不同的url，缓存到不同的文件中
+#
+# 8 还记得我们用函数对象的概念，制作一个函数字典的操作吗，来来来，我们有更高大上的做法，
+# 在文件开头声明一个空字典，然后在每个函数前加上装饰器，完成自动添加到字典的操作
+#
+# 9 编写日志装饰器，实现功能如：一旦函数f1执行，则将消息2017-07-21 11:12:11 f1 run写入到日志文件中，日志文件路径可以指定
+# 注意：时间格式的获取
+# import time
+# time.strftime('%Y-%m-%d %X')
+# 参考答案
+
+
+# 题目4：
 db='db.txt'
 login_status={'user':None,'status':False}
 def auth(auth_type='file'):
@@ -37,22 +96,12 @@ def home(name):
     print('welcome %s to home' %name)
 
 
-# index()
-# home('albert')
-
-#题目五
+index()
+home('albert')
+#
+# #题目五
 import time,random
 user={'user':None,'login_time':None,'timeout':0.000003,}
-
-def timmer(func):
-    def wrapper(*args,**kwargs):
-        s1=time.time()
-        res=func(*args,**kwargs)
-        s2=time.time()
-        print('%s' %(s2-s1))
-        return res
-    return wrapper
-
 
 def auth(func):
     def wrapper(*args,**kwargs):
@@ -75,15 +124,15 @@ def index():
     print('welcome to index')
 
 @auth
-def home(name):
+def home():
     time.sleep(random.randrange(3))
-    print('welcome %s to home ' %name)
+    print('welcome %s to home ')
 
 index()
 home('albert')
-
-#题目六:略
-#题目七：简单版本
+#
+#
+#题目6,7
 import requests
 import os
 cache_file='cache.txt'
@@ -107,62 +156,12 @@ def get(url):
     return requests.get(url).text
 
 
-# res=get('https://www.python.org')
+res=get('https://www.python.org')
 
-# print(res)
-
-#题目七:扩展版本
-import requests,os,hashlib
-engine_settings={
-    'file':{'dirname':'./db'},
-    'mysql':{
-        'host':'127.0.0.1',
-        'port':3306,
-        'user':'root',
-        'password':'123'},
-    'redis':{
-        'host':'127.0.0.1',
-        'port':6379,
-        'user':'root',
-        'password':'123'},
-}
-
-def make_cache(engine='file'):
-    if engine not in engine_settings:
-        raise TypeError('engine not valid')
-    def deco(func):
-        def wrapper(url):
-            if engine == 'file':
-                m=hashlib.md5(url.encode('utf-8'))
-                cache_filename=m.hexdigest()
-                cache_filepath=r'%s/%s' %(engine_settings['file']['dirname'],cache_filename)
-
-                if os.path.exists(cache_filepath) and os.path.getsize(cache_filepath):
-                    return open(cache_filepath,encoding='utf-8').read()
-
-                res=func(url)
-                with open(cache_filepath,'w',encoding='utf-8') as f:
-                    f.write(res)
-                return res
-            elif engine == 'mysql':
-                pass
-            elif engine == 'redis':
-                pass
-            else:
-                pass
-
-        return wrapper
-    return deco
-
-@make_cache(engine='file')
-def get(url):
-    return requests.get(url).text
-
-# print(get('https://www.python.org'))
-print(get('https://www.baidu.com'))
+print(res)
 
 
-#题目八
+# #题目八
 route_dic={}
 
 def make_route(name):
@@ -186,8 +185,8 @@ def func4():
     print('delete')
 
 print(route_dic)
-
-
+#
+#
 #题目九
 import time
 import os
@@ -208,5 +207,5 @@ def logger(logfile):
 @logger(logfile='aaaaaaaaaaaaaaaaaaaaa.log')
 def index():
     print('index')
-
-index()
+#
+#index()
